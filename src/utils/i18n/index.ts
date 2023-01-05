350ef81dcb05139d68f7f5c2c0b1a95d2773bb7e
Augusto Daniel <augustodanieltec@gmail.com>
2023-01-05 09:35:17 -0300

    First commit, add theme change and confi of i18n

---
+++ src/utils/i18n/index.ts
@@ -0,0 +1,19 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18next.use(initReactI18next).init({
+    fallbackLng: "en",
+    interpolation: {
+        escapeValue: false
+    },
+    resources: {
+        en:{
+            
+        },
+        fr: {
+            transalation: {
+                welcome: "To Franch"
+            }
+        },
+    }
+})

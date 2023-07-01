133b2573911a44f8a1209219673b1ea33436189e
Augusto Daniel <augustodanieltec@gmail.com>
2023-07-01 03:49:36 -0300

    fix: Adjust build

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from 'styled-components';
+import React from 'react';
+// import { ThemeContext } from 'styled-components';
 
 import { Container } from './style';
 import 'react-toggle/style.css';
@@ -18,7 +18,7 @@ interface Props {
 }
 
 const HomePage: React.FC<Props> = () => {
-  const { title } = useContext(ThemeContext);
+  // const { title } = useContext(ThemeContext);
   
   // const githubIcon = title === 'dark' ?  GitHubDark : GitHub;
 
